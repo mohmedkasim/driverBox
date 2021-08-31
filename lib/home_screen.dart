ee98e69b7243163e6d8702ffb6515d030f4e257c
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2021-08-30 19:50:45 -1200

    change padding

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:trock_driver/components/logo_container.dart';
 import 'package:trock_driver/components/user_account_container.dart';
-import 'package:trock_driver/constents.dart';
 
 import 'components/total_delivered_container.dart';
+import 'constents.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -17,35 +17,98 @@ class _HomeScreenState extends State<HomeScreen> {
       color: Colors.white,
       child: SafeArea(
         child: Scaffold(
-          body: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
-            child: Column(
-              children: [
-                Directionality(
-                  textDirection: TextDirection.ltr,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [LogoContainer(), UserContainer()],
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                ),
-                TextField(
-                  decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.search),
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(50.0),
+          body: Column(
+            children: [
+              Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                  child: Column(
+                    children: [
+                      Directionality(
+                        textDirection: TextDirection.ltr,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [LogoContainer(), UserContainer()],
+                        ),
                       ),
-                      filled: true,
-                      hintStyle: TextStyle(color: Colors.grey[600]),
-                      hintText: "بحث عن متاجر...",
-                      fillColor: Colors.white70),
+                      SizedBox(
+                        height: 30,
+                      ),
+                      TextField(
+                        decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.search),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(50.0),
+                            ),
+                            filled: true,
+                            hintStyle: TextStyle(color: Colors.grey[600]),
+                            hintText: "بحث عن متاجر...",
+                            fillColor: Colors.white70),
+                      ),
+                      SizedBox(height: 30),
+                      TotalDeliveredContainer(),
+                      SizedBox(height: 30),
+                      Row(
+                        children: [
+                          Text(
+                            'جدول ',
+                            style: TextStyle(fontSize: 20),
+                          ),
+                          Text(
+                            'اليوم',
+                            style: TextStyle(
+                                fontSize: 20,
+                                color: cPrimaryColor,
+                                fontWeight: FontWeight.bold),
+                          )
+                        ],
+                      ),
+                      SizedBox(height: 15),
+                    ],
+                  )),
+              Container(
+                height: 120,
+                child: ListView.separated(
+                  scrollDirection: Axis.horizontal,
+                  shrinkWrap: true,
+                  primary: false,
+                  itemCount: 4,
+                  separatorBuilder: (context, int index) {
+                    return SizedBox(
+                      width: 5,
+                    );
+                  },
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(20),
+                            color: cCardColor),
+                        child: Padding(
+                          padding: const EdgeInsets.all(20.0),
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "12:00 PM",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 18),
+                              ),
+                              SizedBox(height: 5),
+                              Text(
+                                "Samsung",
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 20),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
                 ),
-                SizedBox(height: 30),
-                TotalDeliveredContainer()
-              ],
-            ),
+              )
+            ],
           ),
         ),
       ),
