95c908c3c80fee4cb5e354c492a87e23025e7cdc
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2021-08-31 17:08:14 -1200

    add new screen
    add place for map, way location from/to, responsible receive packages, list of package

---
+++ lib/screens/order_request_screen.dart
@@ -0,0 +1,199 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_dash/flutter_dash.dart';
+import 'package:trock_driver/components/list_view_item.dart';
+import 'package:trock_driver/constents.dart';
+
+class OrderRequestScreen extends StatefulWidget {
+  @override
+  _OrderRequestScreenState createState() => _OrderRequestScreenState();
+}
+
+class _OrderRequestScreenState extends State<OrderRequestScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey,
+      child: SafeArea(
+        child: Scaffold(
+            backgroundColor: Color.fromRGBO(244, 244, 250, 1),
+            body: SingleChildScrollView(
+              child: Directionality(
+                textDirection: TextDirection.rtl,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      color: Colors.grey,
+                      height: MediaQuery.of(context).size.height / 3,
+                      child: Stack(
+                        children: [
+                          Container(
+                            height: MediaQuery.of(context).size.height / 3,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Align(
+                              alignment: Alignment.topLeft,
+                              child: GestureDetector(
+                                onTap: () => Navigator.pop(context),
+                                child: Container(
+                                  child: CircleAvatar(
+                                    child: Icon(
+                                      Icons.arrow_forward,
+                                      color: Colors.black,
+                                    ),
+                                    backgroundColor: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Align(
+                            alignment: Alignment.bottomCenter,
+                            child: Container(
+                              height: 30,
+                              decoration: BoxDecoration(
+                                  color: Color.fromRGBO(244, 244, 250, 1),
+                                  borderRadius: BorderRadius.only(
+                                      topLeft: Radius.circular(20),
+                                      topRight: Radius.circular(20))),
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(height: 10),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Row(
+                                children: [
+                                  Icon(
+                                    Icons.my_location,
+                                    color: cPrimaryColor,
+                                  ),
+                                  SizedBox(width: 5),
+                                  Text('طريق المطار، مخزن'),
+                                ],
+                              ),
+                              Container(
+                                  decoration: BoxDecoration(
+                                      border: Border.all(color: Colors.black26),
+                                      borderRadius: BorderRadius.circular(5)),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(3.0),
+                                    child: Text(
+                                      '2021-02-12, 9:00 PM',
+                                      style: TextStyle(color: Colors.black26),
+                                    ),
+                                  )),
+                            ],
+                          ),
+                          SizedBox(height: 5),
+                          Padding(
+                            padding: const EdgeInsets.only(right: 12.0),
+                            child: Dash(
+                                direction: Axis.vertical,
+                                length: 30,
+                                dashLength: 5,
+                                dashColor: Colors.grey),
+                          ),
+                          SizedBox(height: 5),
+                          Row(
+                            children: [
+                              Icon(Icons.location_on, color: Colors.red),
+                              SizedBox(width: 5),
+                              Text('الليثي، شركة تك كيوب'),
+                            ],
+                          ),
+                          SizedBox(height: 20),
+                          Container(
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(10),
+                              color: Colors.white,
+                            ),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Row(
+                                    children: [
+                                      Container(
+                                        decoration: BoxDecoration(
+                                            borderRadius:
+                                                BorderRadius.circular(10),
+                                            color: cPrimaryColor),
+                                        child: Padding(
+                                            padding: const EdgeInsets.all(8.0),
+                                            child: Image.asset(
+                                              'assets/images/box.png',
+                                              width: 40,
+                                            )),
+                                      ),
+                                      SizedBox(
+                                        width: 10,
+                                      ),
+                                      Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text('محمد صالح'),
+                                          SizedBox(height: 5),
+                                          Text(
+                                            'مسؤول استقبال الشحنة، تك كيوب',
+                                            style: TextStyle(
+                                                color: Colors.grey,
+                                                fontSize: 10),
+                                          ),
+                                        ],
+                                      ),
+                                    ],
+                                  ),
+                                  CircleAvatar(
+                                    backgroundColor: Colors.red,
+                                    child: Icon(
+                                      Icons.call,
+                                      color: Colors.white,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Text(
+                            'البضائع',
+                            style: TextStyle(fontSize: 18),
+                          ),
+                          SizedBox(height: 5),
+                          Container(
+                            height: 150,
+                            child: ListView.builder(
+                                scrollDirection: Axis.horizontal,
+                                primary: false,
+                                shrinkWrap: true,
+                                itemCount: 5,
+                                itemBuilder: (context, index) {
+                                  return ListViewItem(
+                                      value1: "تلفاز 12 بوصة",
+                                      value2: "الكمية 5");
+                                }),
+                          )
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )),
+      ),
+    );
+  }
+}
