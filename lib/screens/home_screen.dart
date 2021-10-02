e09f860f4e3f3c80eeaf6eacc45961e694e4986a
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2021-10-02 05:33:57 -0500

    add directionality

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -17,142 +17,147 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
-      child: SafeArea(
-        child: Scaffold(
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                Padding(
-                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                    child: Column(
-                      children: [
-                        Directionality(
-                          textDirection: TextDirection.ltr,
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Directionality(
+      textDirection: TextDirection.rtl,
+      child: Container(
+        color: Colors.white,
+        child: SafeArea(
+          child: Scaffold(
+            body: SingleChildScrollView(
+              child: Column(
+                children: [
+                  Padding(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                      child: Column(
+                        children: [
+                          Directionality(
+                            textDirection: TextDirection.ltr,
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: [
+                                LogoContainer(),
+                                UserContainer(),
+                              ],
+                            ),
+                          ),
+                          SizedBox(
+                            height: 30,
+                          ),
+                          TextField(
+                            decoration: InputDecoration(
+                                prefixIcon: Icon(Icons.search),
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(50.0),
+                                ),
+                                filled: true,
+                                hintStyle: TextStyle(color: Colors.grey[600]),
+                                hintText: "بحث عن متاجر...",
+                                fillColor: Colors.white70),
+                          ),
+                          SizedBox(height: 30),
+                          TotalDeliveredContainer(),
+                          SizedBox(height: 30),
+                          Row(
                             children: [
-                              LogoContainer(),
-                              UserContainer(),
+                              Text(
+                                'جدول ',
+                                style: TextStyle(fontSize: 20),
+                              ),
+                              Text(
+                                'اليوم',
+                                style: TextStyle(
+                                    fontSize: 20,
+                                    color: cPrimaryColor,
+                                    fontWeight: FontWeight.bold),
+                              )
                             ],
                           ),
-                        ),
-                        SizedBox(
-                          height: 30,
-                        ),
-                        TextField(
-                          decoration: InputDecoration(
-                              prefixIcon: Icon(Icons.search),
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(50.0),
-                              ),
-                              filled: true,
-                              hintStyle: TextStyle(color: Colors.grey[600]),
-                              hintText: "بحث عن متاجر...",
-                              fillColor: Colors.white70),
-                        ),
-                        SizedBox(height: 30),
-                        TotalDeliveredContainer(),
-                        SizedBox(height: 30),
-                        Row(
-                          children: [
-                            Text(
-                              'جدول ',
-                              style: TextStyle(fontSize: 20),
-                            ),
-                            Text(
-                              'اليوم',
-                              style: TextStyle(
-                                  fontSize: 20,
-                                  color: cPrimaryColor,
-                                  fontWeight: FontWeight.bold),
-                            )
-                          ],
-                        ),
-                      ],
-                    )),
-                Container(
-                  height: 120,
-                  child: ListView.separated(
-                    scrollDirection: Axis.horizontal,
-                    shrinkWrap: true,
-                    primary: false,
-                    itemCount: 4,
-                    separatorBuilder: (context, int index) {
-                      return SizedBox(
-                        width: 5,
-                      );
-                    },
-                    itemBuilder: (BuildContext context, int index) {
-                      return ListViewItem(
-                        value1: "PM 12:00",
-                        value2: "Samsung",
-                        onPressed: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => LoadingItemsScreen()));
-                        },
-                      );
-                    },
-                  ),
-                ),
-                SizedBox(height: 10),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
-                  child: MyScheduleContainer(),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    right: 30,
-                    left: 30,
-                    top: 15,
-                  ),
-                  child: Row(
-                    children: [
-                      Text(
-                        'طلبات ',
-                        style: TextStyle(fontSize: 20),
-                      ),
-                      Text(
-                        'توصيل',
-                        style: TextStyle(
-                            fontSize: 20,
-                            color: cPrimaryColor,
-                            fontWeight: FontWeight.bold),
-                      )
-                    ],
-                  ),
-                ),
-                SizedBox(height: 8),
-                Container(
-                  height: 120,
-                  child: ListView.separated(
-                    scrollDirection: Axis.horizontal,
-                    shrinkWrap: true,
-                    primary: false,
-                    itemCount: 4,
-                    separatorBuilder: (context, int index) {
-                      return SizedBox(
-                        width: 5,
-                      );
-                    },
-                    itemBuilder: (BuildContext context, int index) {
-                      return ListViewItem(
-                          value1: "2021-02-12",
-                          value2: "9:00 PM",
+                        ],
+                      )),
+                  Container(
+                    height: 120,
+                    child: ListView.separated(
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      primary: false,
+                      itemCount: 4,
+                      separatorBuilder: (context, int index) {
+                        return SizedBox(
+                          width: 5,
+                        );
+                      },
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListViewItem(
+                          value1: "PM 12:00",
+                          value2: "Samsung",
                           onPressed: () {
                             Navigator.push(
                                 context,
                                 MaterialPageRoute(
                                     builder: (context) =>
-                                        OrderRequestScreen()));
-                          });
-                    },
+                                        LoadingItemsScreen()));
+                          },
+                        );
+                      },
+                    ),
                   ),
-                ),
-              ],
+                  SizedBox(height: 10),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                    child: MyScheduleContainer(),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(
+                      right: 30,
+                      left: 30,
+                      top: 15,
+                    ),
+                    child: Row(
+                      children: [
+                        Text(
+                          'طلبات ',
+                          style: TextStyle(fontSize: 20),
+                        ),
+                        Text(
+                          'توصيل',
+                          style: TextStyle(
+                              fontSize: 20,
+                              color: cPrimaryColor,
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 8),
+                  Container(
+                    height: 120,
+                    child: ListView.separated(
+                      scrollDirection: Axis.horizontal,
+                      shrinkWrap: true,
+                      primary: false,
+                      itemCount: 4,
+                      separatorBuilder: (context, int index) {
+                        return SizedBox(
+                          width: 5,
+                        );
+                      },
+                      itemBuilder: (BuildContext context, int index) {
+                        return ListViewItem(
+                            value1: "2021-02-12",
+                            value2: "9:00 PM",
+                            onPressed: () {
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) =>
+                                          OrderRequestScreen()));
+                            });
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
