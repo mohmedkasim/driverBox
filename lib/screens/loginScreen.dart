2638572920b076ecebf3265b5669450926b5bcf7
mohmedkasim <56302970+mohmedkasim@users.noreply.github.com>
2021-10-02 00:43:39 -0500

    add loginScreen

---
+++ lib/screens/loginScreen.dart
@@ -0,0 +1,104 @@
+import 'package:flutter/material.dart';
+import 'package:trock_driver/components/curvePainter.dart';
+import 'package:trock_driver/constents.dart';
+import 'package:trock_driver/screens/home_screen.dart';
+
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Container(
+              child: Stack(
+                alignment: Alignment.center,
+                children: [
+                  CustomPaint(
+                    size: Size(MediaQuery.of(context).size.width,
+                        MediaQuery.of(context).size.height / 3),
+                    painter: CurvePainter(),
+                  ),
+                  Text(
+                    '.DriverTo',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontFamily: "pacifico",
+                        fontSize: 30),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Column(
+                children: [
+                  Text(
+                    'Welcome!',
+                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                  ),
+                  Text('Sing in to continue'),
+                  SizedBox(height: 20),
+                  TextField(
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.account_circle_rounded),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50.0),
+                          borderSide:
+                              const BorderSide(color: Colors.white, width: 0.0),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                        filled: true,
+                        hintStyle: TextStyle(color: Colors.grey[600]),
+                        hintText: "اسم المستخدم",
+                        fillColor: Colors.grey[300]),
+                  ),
+                  SizedBox(height: 15),
+                  TextField(
+                    decoration: InputDecoration(
+                        prefixIcon: Icon(Icons.lock),
+                        enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50.0),
+                          borderSide:
+                              const BorderSide(color: Colors.white, width: 0.0),
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(50.0),
+                        ),
+                        filled: true,
+                        hintStyle: TextStyle(color: Colors.grey[600]),
+                        hintText: "كلمة المرور",
+                        fillColor: Colors.grey[300]),
+                  ),
+                  SizedBox(height: 30),
+                  Container(
+                    width: 200,
+                    child: RaisedButton(
+                      color: cPrimaryColor,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      onPressed: () => Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => HomeScreen())),
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 10),
+                        child: Text(
+                          'دخول',
+                          style: TextStyle(color: Colors.white, fontSize: 20),
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
